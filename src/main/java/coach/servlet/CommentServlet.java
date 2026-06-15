5ca8035dd03330534d184761c2a2718731211c6c
你的名字 <>
2026-06-15 23:43:23 +0800

    feat: 教练模块前端页面与Servlet骨架

--- src/main/java/coach/servlet/CommentServlet.java
+++ src/main/java/coach/servlet/CommentServlet.java
@@ -1,4 +1,17 @@
 package coach.servlet;
 
-public class CommentServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/coach/comments")
+public class CommentServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.write("{\"code\":200,\"data\":[{\"studentName\":\"李四\",\"studentScore\":5,\"comment\":\"教得非常好\"}]}");
+    }
 }
--- src/main/java/coach/servlet/ScoreServlet.java
+++ src/main/java/coach/servlet/ScoreServlet.java
@@ -1,4 +1,18 @@
 package coach.servlet;
 
-public class ScoreServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/coach/pending")
+public class ScoreServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        // TODO: 调用service查询待评分记录，暂时返回模拟数据
+        out.write("{\"code\":200,\"data\":[{\"bookingId\":\"1\",\"studentName\":\"张三\",\"timeSlot\":\"2025-06-20 09:00-10:00\"}]}");
+    }
 }
